56a9aef72cf21d8963d537e2dea06d557f22261a
Alessandro Tegner <alessandro.tegner@gmail.com>
2018-07-03 20:44:03 -0300

    Load development env if exists

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,2 +1,17 @@
-alias rs='bin/rails s'
-alias rc='bin/rails c'
+_load_project_env() {
+  if [ -f development.env ]; then
+    source development.env
+  fi
+}
+
+rs() {
+  _load_project_env
+
+  ./bin/rails s
+}
+
+rc() {
+  _load_project_env
+
+  ./bin/rails c
+}
