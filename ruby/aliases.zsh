b7b9b37d195d05dc757808328ff1c5febf42480f
Alessandro Tegner <alessandro.tegner@gmail.com>
2019-02-13 17:50:31 -0200

    Remove :poop: env loader

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,19 +1,3 @@
-_load_project_env() {
-  if [ -f development.env ]; then
-    cat development.env | while read line; do
-      export $line
-    done
-  fi
-}
-
-rs() {
-  _load_project_env
-
-  ./bin/rails s
-}
-
-rc() {
-  _load_project_env
-
-  ./bin/rails c
-}
+alias rs='bin/rails s'
+alias rc='bin/rails c'
+alias rspec='bundle exec rspec'
