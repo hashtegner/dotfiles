7ca3ab1c9cd83f5dfd2dfca858063a17da6d364f
Alessandro Tegner <alessandro.tegner@gmail.com>
2020-02-10 18:38:20 -0300

    Setup nodejs and global languages

--- asdf/install.sh
+++ asdf/install.sh
@@ -11,10 +11,17 @@ $HOME/.asdf/bin/asdf plugin-update --all
 
 $HOME/.asdf/bin/asdf install ruby 2.6.5
 $HOME/.asdf/bin/asdf install ruby 2.7.0
+$HOME/.asdf/bin/asdf global ruby 2.6.5
 
 $HOME/.asdf/bin/asdf install golang 1.13.7
+$HOME/.asdf/bin/asdf global golang 1.13.7
 
 export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl)"
 $HOME/.asdf/bin/asdf install erlang 22.2.6
 $HOME/.asdf/bin/asdf install elixir 1.10.1
+$HOME/.asdf/bin/asdf global erlang 22.2.6
+$HOME/.asdf/bin/asdf global elixir 1.10.1
 
+bash $HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring
+$HOME/.asdf/bin/asdf install nodejs 12.15.0
+$HOME/.asdf/bin/asdf global nodejs 12.15.0
