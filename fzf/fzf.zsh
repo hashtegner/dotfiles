6090e489a63d4af2cfaa73ed688cfb173fdc338d
Alessandro Tegner <alessandro.tegner@gmail.com>
2020-05-16 16:31:47 -0300

    Setup gruvbox fzf theme

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,2 +1,8 @@
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+ --color=fg:#a89984,bg:#1d2021,hl:#cc241d
+ --color=fg+:#bdae93,bg+:#1d2021,hl+:#cc241d
+ --color=info:#d65d0e,prompt:#689d6a,pointer:#d65d0e
+ --color=marker:#d79921,spinner:#98971a,header:#458588'
