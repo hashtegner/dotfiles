42d9e44ae4d35faa639fe6960bb0513a3dc8f72e
Alessandro Tegner <alessandro.tegner@gmail.com>
2020-05-18 08:51:24 -0300

    Upgrade default theme

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -2,7 +2,6 @@ export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
- --color=fg:#a89984,bg:#1d2021,hl:#cc241d
- --color=fg+:#bdae93,bg+:#1d2021,hl+:#cc241d
- --color=info:#d65d0e,prompt:#689d6a,pointer:#d65d0e
- --color=marker:#d79921,spinner:#98971a,header:#458588'
+--color fg:242,bg:233,hl:65,fg+:15,bg+:234,hl+:108
+--color info:108,prompt:109,spinner:108,pointer:168,marker:168
+'
