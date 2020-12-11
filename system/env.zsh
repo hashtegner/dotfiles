08a57b23837544c7cd0808a35d01f241d27cfe5c
Alessandro Tegner <alessandro.tegner@gmail.com>
2020-12-11 09:16:38 -0300

    Back to the nvim

--- system/env.zsh
+++ system/env.zsh
@@ -1,8 +1,7 @@
 export EDITOR='nvim'
 export VISUAL='$EDITOR'
-export GIT_EDITOR='$EDITOR -f'
+export GIT_EDITOR='$EDITOR -w'
 export GPG_TTY=$(tty)
 
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 alias e="$EDITOR"
-alias code="$EDITOR"
