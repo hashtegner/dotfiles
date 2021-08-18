8b1a33f518fc79c2dab39aa26c259bebeb69c71a
Alessandro Tegner <alessandro.tegner@gmail.com>
2021-08-18 08:49:43 -0300

    Update asdf script

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,4 +1,4 @@
-[ -f $HOME/.asdf/asdf.sh ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.6
+[ -f $HOME/.asdf/asdf.sh ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
 
 
 $HOME/.asdf/bin/asdf update
@@ -10,19 +10,4 @@ $HOME/.asdf/bin/asdf plugin-add elixir
 $HOME/.asdf/bin/asdf plugin-add rust
 $HOME/.asdf/bin/asdf plugin-update --all
 
-$HOME/.asdf/bin/asdf install ruby 2.6.6
-$HOME/.asdf/bin/asdf install ruby 2.7.0
-$HOME/.asdf/bin/asdf global ruby 2.6.6
-
-$HOME/.asdf/bin/asdf install golang 1.14.2
-$HOME/.asdf/bin/asdf global golang 1.14.2
-
-# export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl)"
-# $HOME/.asdf/bin/asdf install erlang 22.2.6
-# $HOME/.asdf/bin/asdf install elixir 1.10.1
-# $HOME/.asdf/bin/asdf global erlang 22.2.6
-# $HOME/.asdf/bin/asdf global elixir 1.10.1
-
 bash $HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring
-$HOME/.asdf/bin/asdf install nodejs 12.16.2
-$HOME/.asdf/bin/asdf global nodejs 12.16.2
