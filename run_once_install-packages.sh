98d9d68f73dce56742346ad1c6fbd6f2992e66d7
Alessandro Tegner <alessandro.tegner@gmail.com>
2022-04-10 22:52:29 -0300

    add some dependencies

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -8,13 +8,16 @@ brew install bat
 brew install chezmoi
 brew install fzf
 brew install git
+brew install mas
 brew install zsh
 
 brew install 1password --cask
 brew install alfred --cask
 brew install appcleaner --cask
-brew install iterm2 --cask
 brew install datagrip --cask
+brew install docker --cask
+brew install intellij-idea-ce --cask
+brew install iterm2 --cask
 brew install paw --cask
 brew install rectangle --cask
 brew install slack-beta --cask
@@ -24,3 +27,10 @@ brew install visual-studio-code --cask
 
 brew tap homebrew/cask-fonts
 brew install font-hasklig --cask
+
+mas install 1352778147 # Bitwarden
+mas install 1462114288 # Grammarly for Safari
+mas install 1529448980 # Reeder 5
+mas install 1429033973 # RunCat
+mas install 1284863847 # Unsplash Wallpapers
+mas install 497799835  # Xcode
