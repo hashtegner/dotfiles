3232b3070560aab546e571ec5e9675d7b81c8737
Alessandro Tegner <alessandro.tegner@gmail.com>
2022-04-11 10:04:33 -0300

    missing packages and configure font

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -2,7 +2,6 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-
 brew install autojump
 brew install bat
 brew install chezmoi
@@ -10,6 +9,7 @@ brew install exa
 brew install fzf
 brew install git
 brew install mas
+brew install neovim
 brew install zsh
 
 brew install 1password --cask
@@ -17,6 +17,7 @@ brew install alfred --cask
 brew install appcleaner --cask
 brew install datagrip --cask
 brew install docker --cask
+brew install google-cloud-sdk --cask
 brew install intellij-idea-ce --cask
 brew install iterm2 --cask
 brew install paw --cask
@@ -26,8 +27,12 @@ brew install spotify --cask
 brew install the-unarchiver --cask
 brew install visual-studio-code --cask
 
+brew tap homebrew/cask-drivers
+brew install logitech-g-hub --cask
+brew install logitech-camera-settings --cask
+
 brew tap homebrew/cask-fonts
-brew install font-firacode-nerd-font --cask
+brew install font-hasklug-nerd-font --cask
 
 mas install 1352778147 # Bitwarden
 mas install 1462114288 # Grammarly for Safari
