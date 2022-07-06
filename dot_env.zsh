eb83a4ee773997111d304b8c6160ecd2f33ab4e8
Alessandro.tegner <alessandro.tegner@gmail.com>
2022-07-06 10:33:55 -0300

    Configure mysql client bins

--- dot_env.zsh
+++ dot_env.zsh
@@ -2,3 +2,5 @@ export EDITOR='code'
 export VISUAL='$EDITOR'
 export GIT_EDITOR='$EDITOR -w'
 export GPG_TTY=$(tty)
+export PATH="/usr/local/opt/mysql-client@5.7/bin:$PATH"
+export PATH="/usr/local/opt/openssl@3/bin:$PATH"
