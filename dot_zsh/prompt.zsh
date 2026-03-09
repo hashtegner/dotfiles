fee9e9c109e2b55ae322ffdd475456b795e80706
Alessandro Tegner <1260049+hashtegner@users.noreply.github.com>
2026-03-08 21:47:27 -0300

    Add prompt :nail_care:

--- dot_zsh/prompt.zsh
+++ dot_zsh/prompt.zsh
@@ -1,7 +1,9 @@
+# Catppuccin Latte
 autoload -Uz vcs_info
 precmd() { vcs_info }
 
 zstyle ':vcs_info:git:*' formats ' %b'
 
 setopt PROMPT_SUBST
-PROMPT='%F{blue}%~%f%F{yellow}${vcs_info_msg_0_}%f %# '
+# Lavender path, Mauve git branch, Green/Red prompt symbol
+PROMPT='%F{#7287fd}%~%f%F{#8839ef}${vcs_info_msg_0_}%f %(#.%F{#d20f39}.%F{#40a02b})❯%f '
