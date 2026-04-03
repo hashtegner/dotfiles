14b0062e23bc472a57d55d223faf92cee94c530a
Alessandro Tegner <1260049+hashtegner@users.noreply.github.com>
2026-04-03 14:48:04 -0300

    Setup psql

--- dot_zsh/path.zsh
+++ dot_zsh/path.zsh
@@ -4,4 +4,5 @@ export PATH="$HOME/.local/bin:$PATH"
 # macOS-specific paths
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
+  export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
 fi
